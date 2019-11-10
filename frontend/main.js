2317ac6bd4a0e22e5eb1586f83b90bf3db47a166
Ravi Kalia <ravkalia@gmail.com>
2019-11-10 10:00:18 -0500

    feature(styling): Add SQL styling

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,50 @@
-const form = document.getElementsByTagName('form');
-// console.log('form', form);
+const form = document.querySelector('form');
+const h4 = document.querySelector('h4');
+const reponseContainer = document.querySelector('.response-container');
+const input = document.querySelector('#nl-question');
+const codeBlock = document.querySelector('.response-container .card-body');
 
-// const postData = (e) => {
-//     e.preventDefault();
-//     console.log('send to google colaboratory')
-// }
+const postData = () => {
+  const url = 'https://jsonplaceholder.typicode.com/posts';
+  
+  fetch(`${url}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title: `${input.value}`,
+      body: 'test',
+      userId: 1
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => response.json())
+  .then(data => displaySqlData(data))
+}
 
-
-
-function sConsole(event) {
-    event.preventDefault();
-    const input = document.querySelector('.question');
-    console.log(input.value);
-    
+const displayData = (e) => {
+  e.preventDefault();
+  h4.textContent = `Results for: -${input.value}`;
+  reponseContainer.style.opacity = 1;
+  reponseContainer.style.transition = 'all .5s ease-in-out';
+  displaySqlData();
   }
 
+const displaySqlData = (data) => {
+  data ? (
+    codeBlock.innerHTML = ` 
+        ${data.title}
+  `
+  ):(`<span>Loading...</span>`);
+}
 
-form.addEventListener('submit', sConsole)
+const clearInput = () => document.querySelector('#nl-question').value = '';
+
+form.addEventListener('submit', postData);
+form.addEventListener('submit', displayData);
+form.addEventListener('submit', clearInput);
+
+input.addEventListener('keydown', () => {
+    reponseContainer.style.opacity = 0;
+    reponseContainer.style.transition = 'all .5s ease-in-out';
+})
