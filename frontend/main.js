3f07c6e66dbdda2f508bb4fbe8983f5ce437fe1d
Ravi Kalia <ravkalia@gmail.com>
2019-12-11 20:54:24 -0500

    feat(frontend): Add functioning buttons

--- frontend/main.js
+++ frontend/main.js
@@ -1,39 +1,47 @@
+// selectors
 const form = document.querySelector('form');
+const h4 = document.querySelector('h4');
+const inputNlQuestion = document.querySelector('#nl-question');
+const inputDbSchema = document.querySelector('#db-schema');
 const sqlPrediction = document.querySelector('#sql-prediction');
 const codeTag = document.querySelector('.monokai-sublime');
 const copyBtn = document.querySelector('.copy-btn');
-const inputNlQuestion = document.querySelector('#nl-question');
-const inputDbSchema = document.querySelector('#db-schema');
 
-const url = 'https://jsonplaceholder.typicode.com/posts/'
+// functions
+const getQuery = (e) => {
 
-
-
-const postData = (e) => {
   e.preventDefault();
-  const postOptions = {
-    method: 'POST',
-    body: JSON.stringify({
-      question: `${inputNlQuestion.value}`,
-      tableSchema: `${inputDbSchema.value}`
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  };
-  fetch(url, postOptions)
-  .then(response => response.json())
-  .then(data => {
-    codeTag.textContent = `${data.question} ${data.tableSchema}`;
-    document.querySelectorAll('pre, code').forEach(block => {
-      hljs.highlightBlock(block);
-    });
-    sqlPrediction.style.opacity = 1;
-    sqlPrediction.style.transition = 'all .5s ease-in-out';
-    inputNlQuestion.value = ''
-    inputDbSchema.value = ''
-  })
-}
+  fetch(`https://jsonplaceholder.typicode.com/posts/${inputNlQuestion.value}`)
+    .then(response => response.json())
+    .then(data => {
+      codeTag.innerHTML = `${JSON.stringify(data)}`;
+      sqlPrediction.style.opacity = 1;
+      sqlPrediction.style.transition = 'all .5s ease-in-out';
+    })    
+  }
+
+// const postData = (e) => {
+//   e.preventDefault();
+//   fetch('https://jsonplaceholder.typicode.com/posts/', {
+//     headers: { "Content-Type": "application/json; charset=utf-8" },
+//     method: 'POST',
+//     body: JSON.stringify({
+//       title: `${ inputNlQuestion.value }`,
+//       body: `${ inputDbSchema.value }`
+//     })
+//   })
+//   .then(response => response.json())
+//   .then(data => {
+//     codeTag.textContent = `${JSON.stringify(data)}`
+//     sqlPrediction.style.opacity = 1;
+//     sqlPrediction.style.transition = 'all .5s ease-in-out';
+//   })
+// }
+
+const clearInput = () => {
+  inputNlQuestion.value = ''
+  inputDbSchema.value = ''
+};
 
 const copyToClipboard = () => {
   const preTagContent = document.querySelector('#pre-sql').textContent;
@@ -45,8 +53,18 @@ const copyToClipboard = () => {
   textArea.classList.add('d-none');
 }
 
+// hook events
+form.addEventListener('submit', getQuery);
+// form.addEventListener('submit', postData);
+form.addEventListener('submit', clearInput);
 
+inputNlQuestion.addEventListener('keydown', () => {
+    sqlPrediction.style.opacity = 0;
+    sqlPrediction.style.transition = 'all .5s ease-in-out';
+})
 
-
-form.addEventListener('submit', postData);
 copyBtn.addEventListener('click', copyToClipboard);
+
+$(document).ready(function() {
+  $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
+});
